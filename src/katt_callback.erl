33b626ce948e588df81ea22ee890fc54db7b3fad
Andrew J. Ayers <andrew.ayers@ubnt.com>
2014-07-31 08:39:03 -0500

    Added parse of JSON in blueprint responses.
    The README claimed white-space didn't matter, but my manual testing
    indicated otherwise. So, I tracked down and added the functionality
    to parse the response text if the content type is json based on the
    existing is_json_content_type/1 function.

--- src/katt_callback.erl
+++ src/katt_callback.erl
@@ -208,8 +208,14 @@ validate_headers(#katt_response{headers=E0}, #katt_response{headers=A0}) ->
 
 %% Bodies are also allowed to be a superset of expected body, if the parseFun
 %% returns a structure.
-validate_body(#katt_response{body=E}, #katt_response{body=A}) ->
-  compare_struct("/body", E, A, ?MATCH_ANY).
+validate_body(#katt_response{body=E}, #katt_response{body=ActBody, headers=ActHeaders}) ->
+  NormalizedHeaders = [{string:to_lower(K),V} || {K,V} <- ActHeaders],
+  ContentType = proplists:get_value("content-type", NormalizedHeaders, ""),
+  B = case is_json_content_type(ContentType) andalso is_binary(ActBody) of
+        true -> parse_json(ActBody);
+        false -> ActBody
+  end,
+  compare_struct("/body", E, B, ?MATCH_ANY).
 
 %% Compare non-empty JSON structured types; defer to simple comparison otherwise
 compare_struct(_ParentKey, ?MATCH_ANY, _A, _Unexpected)                     ->
